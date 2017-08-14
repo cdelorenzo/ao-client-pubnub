77ee7e9b4565231316bbe34979f08e1835b41790
Chris De Lorenzo <delorenzoc@tabcorp.com.au>
2017-08-14 18:02:59 +1000

    Small Cleanup

--- subscribe.js
+++ subscribe.js
@@ -6,33 +6,42 @@ var subscribers = [];
 
 var subscribe = function(callback) {
 
-	pubnub = new PubNub({
-		publishKey : 'demo',
-		subscribeKey : 'demo'
-	})
+  pubnub = new PubNub({
+    publishKey : 'demo',
+    subscribeKey : 'demo'
+  })
+  
+  pubnub.addListener({
+    message : function(message) {
+      console.log("New Message!!", message);
+    }
+  })
 
-	pubnub.addListener({
-		message : function(message) {
-			console.log("New Message!!", message);
-		}
-	})
+  console.log("Subscribing..");
+  pubnub.subscribe({
+    channels : [ 'test' ]
+  });
 
-	console.log("Subscribing..");
-	pubnub.subscribe({
-		channels : [ 'test' ]
-	});
-
-	callback(null,"Array of parallel connections");
+  callback(null,"Array of parallel connections");
 
 };
 
 if (require.main === module) {
-	subscribers.push(subscribe);
-	//test two connections
-	subscribers.push(subscribe);
-	async.parallel(subscribers, function(err, result){
-		console.log(result)
-	})
+  // Number of connections to run on server.
+  var connections = 10;
+  // Number of seconds to ramp up over.
+  var ramp = 5;
+  // Concurrent connections attempted per second.
+  var rate = Math.ceil(connections / ramp);
+
+  var subscribers = [];
+
+  for (i=0; i < connections; i++) {
+	  subscribers.push(subscribe);
+  }
+  async.parallel(subscribers, function(err, result){
+    console.log(result)
+  });
 } else {
-	console.log('please start via node subscribe.js');
+  console.log('please start via node subscribe.js');
 }
