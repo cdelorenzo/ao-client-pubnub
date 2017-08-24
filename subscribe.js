282d8511252cdb9c65e262def01d343b376d3b21
Chris De Lorenzo <delorenzoc@tabcorp.com.au>
2017-08-24 20:01:33 +1000

    hard code origin

--- subscribe.js
+++ subscribe.js
@@ -17,7 +17,7 @@ var subscribe = function(subID) {
       subscribeKey: config.subscribeKey,
       ssl: true,
       keepAlive: true,
-      origin: config.origin,
+      origin: 'ao10.pubnub.net',
       keepAliveSettings: {
         maxSockets: 100,
         maxFreeSockets: 10,
